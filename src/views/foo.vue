9873b56b85605cde93360f2147be2c812b6df7f7
RongYan Zhang <kawai566@yeah.net>
2017-09-14 17:01:15 +0800

    foo页面

---
+++ src/views/foo.vue
@@ -0,0 +1,48 @@
+<style scoped lang="less">
+    .index{
+        width: 100%;
+        //position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        text-align: center;
+        h1{
+            height: 150px;
+            img{
+                height: 100%;
+            }
+        }
+        h2{
+            color: #666;
+            margin-bottom: 200px;
+            p{
+                margin: 0 0 50px;
+            }
+        }
+        .ivu-row-flex{
+            height: 100%;
+        }
+    }
+</style>
+<template>
+    <div class="index">
+        <ivtable></ivtable>
+    </div>
+</template>
+<script>
+
+import Ivtable from '../components/ivtable.vue'
+export default {
+    components: {
+        Ivtable
+    },
+        methods: {
+            handleStart () {
+                this.$Modal.info({
+                    title: 'Bravo',
+                    content: 'Now, enjoy the convenience of iView.'
+                });
+            }
+        }
+    }
+</script>
