d3bd4af181c053c2054b0da8d995d9a6ee5fd137
RongYan Zhang <kawai566@yeah.net>
2017-09-14 17:00:23 +0800

    添加路由

--- src/router.js
+++ src/router.js
@@ -5,6 +5,29 @@ const routers = [
             title: ''
         },
         component: (resolve) => require(['./views/index.vue'], resolve)
+    },
+    {
+        path: '/grid',
+        meta: {
+            title: 'Grid'
+        },
+        component: (resolve) => require(['./views/grid.vue'], resolve)
+    },
+    {
+        path: '/foo',
+        name: 'foo',
+        meta: {
+            title: 'Foo'
+        },
+        component: (resolve) => require(['./views/foo.vue'], resolve)
+    },
+    {
+        path: '/bar',
+        name: 'bar',
+        meta: {
+            title: 'Bar'
+        },
+        component: (resolve) => require(['./views/bar.vue'], resolve)
     }
 ];
 export default routers;
