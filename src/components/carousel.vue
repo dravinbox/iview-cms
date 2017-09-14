dc08c68ffe2aa75e2bab9204cf11bdbba9650653
RongYan Zhang <kawai566@yeah.net>
2017-09-14 17:00:44 +0800

    走马灯

---
+++ src/components/carousel.vue
@@ -0,0 +1,34 @@
+<style>
+.demo-carousel {
+    height: 200px;
+    line-height: 200px;
+    overflow: hidden;
+    background-color: palegoldenrod;
+}
+</style>
+
+<template>
+    <Carousel autoplay v-model="value2">
+        <Carousel-item>
+            <div class="demo-carousel">1</div>
+        </Carousel-item>
+        <Carousel-item>
+            <div class="demo-carousel">2</div>
+        </Carousel-item>
+        <Carousel-item>
+            <div class="demo-carousel">3</div>
+        </Carousel-item>
+        <Carousel-item>
+            <div class="demo-carousel">4</div>
+        </Carousel-item>
+    </Carousel>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            value2: 0
+        }
+    }
+}
+</script>
